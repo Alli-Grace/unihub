5a5ef27117dad08fe8889893593c1ac2d8968f74
Grace Alli <alligrace56@gmail.com>
2025-05-08 00:57:54 +0100

    Update Models

--- unihub/main/models.py
+++ unihub/main/models.py
@@ -13,19 +13,19 @@ from django.db.models import JSONField
 # User Manager
 # ----------------------------
 class UserManager(BaseUserManager):
-    def create_user(self, email, full_name, password=None, **extra_fields):
+    def create_user(self, email, password=None, **extra_fields):
         if not email:
             raise ValueError("Email is required")
         email = self.normalize_email(email)
-        user = self.model(email=email, full_name=full_name, **extra_fields)
+        user = self.model(email=email,  **extra_fields)
         user.set_password(password)
         user.save(using=self._db)
         return user
 
-    def create_superuser(self, email, full_name, password=None, **extra_fields):
+    def create_superuser(self, email, password=None, **extra_fields):
         extra_fields.setdefault('is_staff', True)
         extra_fields.setdefault('is_superuser', True)
-        return self.create_user(email, full_name, password, **extra_fields)
+        return self.create_user(email, password, **extra_fields)
 
 # ----------------------------
 # User Model
